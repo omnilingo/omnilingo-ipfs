217010792da5f9e5a41a5f05a538c3a9db5706a2
Francis Tyers <ftyers@prompsit.com>
2021-06-23 20:40:32 +0100

    update code

--- importer.py
+++ importer.py
@@ -10,15 +10,12 @@ import re
 import sys
 
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3
+from mutagen.easyid3 import EasyID3
 
 class Importer:
 
 	def __init__(self):
-
-		self.widgets = [' [', progressbar.Timer(format= 'elapsed time: %(elapsed)s'), '] ', 
-			progressbar.Bar('#'),' (', progressbar.ETA(), ') ', ]
-  
+		"""Set up a connection to the local IPFS node"""
 		try:
 			self._client = ipfshttpclient.connect(session=True)
 		except:
@@ -34,53 +31,64 @@ class Importer:
 		res = sum( buf.count(b'\n') for buf in bufgen )
 		f.close()
 		return res
-	
+
 	def path_join(self, *args, sep='/'):
 		"""
 			Join a sequence of arguments on a given delimiter
 			*args: any number of strings
-			sep: directory separator 
+			sep: directory separator
 		"""
 		return sep.join(args)
-	
+
 	def index(self, input_path, output_path):
 		"""
 			Import a Common Voice dump into IPFS
 			input_path: path to a Common Voice dump directory
 			output_path: place to put the generated index in JSON
-		"""	
-	
+		"""
+
 		clip_index = {}
-	
+
 		print(input_path)
 		print(output_path)
-	
+
 		validated_path = self.path_join(input_path, 'validated.tsv')
 		clips_path = self.path_join(input_path, 'clips')
-	
+
 		file_length = self.line_count(validated_path)
 
 		with open(validated_path, newline='') as validated_file:
 			reader = csv.DictReader(validated_file, delimiter='\t')
-			bar = progressbar.ProgressBar(max_value=file_length, widgets=self.widgets).start()
+			bar = progressbar.ProgressBar(max_value=file_length).start()
 			for (i, row) in enumerate(reader):
+				sentence = {
+					'content': row['sentence'],
+					'language': row["locale"],
+					'copyright': "CC0-1.0"
+				}
+				sent_hash = self._client.add_str(sentence, opts={'only_hash':True})
 				clip_path = self.path_join(clips_path, row['path'])
-				clip_res = self._client.add(clip_path, only_hash=True)			
-				sent_hash = self._client.add_str(row['sentence'], opts={'only_hash':True})			
+				audio = EasyID3(clip_path)
+				audio["copyright"] = "CC0-1.0"
+				audio["language"] = row["locale"]
+				audio["album"] = sent_hash
+				audio["author"] = row["client_id"]
+				audio.save()
+				clip_res = self._client.add(clip_path, only_hash=True)
 
-		#		print('c:',clip_res)
-		#		print('s:',sent_hash)
 				if sent_hash not in clip_index:
 					clip_index[sent_hash] = []
 				clip_index[sent_hash].append(clip_res['Hash'])
 				bar.update(i)
-				
+
+		# Save the transcript → clip hash as a json file
 		with open(output_path, 'w') as output_file:
 			json.dump(clip_index, output_file)
 
 	def close(self):
+		"""Close the TCP connection to IPFS"""
 		self._client.close()
-	
+
 if __name__ == "__main__":
 	imp = Importer()
 	imp.index(sys.argv[1], sys.argv[2])
